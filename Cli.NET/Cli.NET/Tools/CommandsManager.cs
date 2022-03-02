968da4f7ace831d484136c03ff154f44a7b3f5b7
EternalQuasar <70824102+EternalQuasar0206@users.noreply.github.com>
2022-03-02 18:27:52 -0300

    chore: development updates

--- Cli.NET/Cli.NET/Tools/CommandsManager.cs
+++ Cli.NET/Cli.NET/Tools/CommandsManager.cs
@@ -5,30 +5,59 @@ namespace Cli.NET.Tools
     public static class CommandsManager
     {
         private static readonly Dictionary<string, IConsoleCommand> _commands = new();
+        private static string _indicator = "Command > ";
+        private static string _notFoundMessage = "Command {x} not found.";
+        private static ConsoleColor _notFoundColor = ConsoleColor.DarkRed;
+        private static ConsoleColor _indicatorColor = ConsoleColor.White;
+
+        /// <summary>
+        /// Register a new command in the commands dictionary.
+        /// </summary>
+        /// <param name="command">The target command</param>
+        /// <param name="commandName">The command name to be called in console</param>
         public static void Register(IConsoleCommand command, string commandName)
         {
             _commands.Add(commandName, command);
         }
 
-        public static void WaitForNextCommand(
-            string indicator,
-            string notFoundMessage,
-            ConsoleColor indicatorColor = ConsoleColor.White,
-            ConsoleColor notFoundColor = ConsoleColor.DarkRed,
-            bool loop = true)
+        public static void SetNotFoundMessage(string message, ConsoleColor color = ConsoleColor.DarkRed)
         {
-            CLNConsole.Write(indicator, indicatorColor);
+            _notFoundMessage = message;
+            _notFoundColor = color;
+        }
+
+        public static void SetIndicator(string indicator, ConsoleColor color = ConsoleColor.White)
+        {
+            _indicator = indicator;
+            _indicatorColor = color;
+        }
+
+        public static void ExecuteEnvironmentCommands()
+        {
+            var commands = Environment.GetCommandLineArgs();
+            foreach (var command in commands)
+            {
+                if (!_commands.ContainsKey(command))
+                {
+                    CLNConsole.WriteLine(_notFoundMessage.Replace("{x}", command), _notFoundColor);
+                }
+            }
+        }
+
+        public static void WaitForNextCommand(bool loop = true)
+        {
+            CLNConsole.Write(_indicator, _indicatorColor);
             string[] input = CLNConsole.ReadText().Split(" ");
 
             if(!_commands.ContainsKey(input[0]))
             {
-                CLNConsole.WriteLine(notFoundMessage.Replace("{x}", input[0]), notFoundColor);
-                if(loop) WaitForNextCommand(indicator, notFoundMessage, indicatorColor, notFoundColor, loop);
+                CLNConsole.WriteLine(_notFoundMessage.Replace("{x}", input[0]), _notFoundColor);
+                if(loop) WaitForNextCommand(loop);
                 return;
             }
 
             _commands[input[0]].Execute(input);
-            if (loop) WaitForNextCommand(indicator, notFoundMessage, indicatorColor, notFoundColor, loop);
+            if (loop) WaitForNextCommand(loop);
         }
     }
 }
